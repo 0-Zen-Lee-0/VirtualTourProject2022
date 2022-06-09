57739e24318358a873a84d82bc00154efac92c9a
Sian Caleb Tolentino Domasig <siancaleb@gmail.com>
2022-06-09 11:05:04 +0800

    Add black panel in video panel to hide others

---
+++ UC Virtual Tour/Assets/Scripts/HoverChangeSprite.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverChangeSprite : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Test");
+    }
+}
